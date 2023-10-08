b43d430db7afe54b019091f5e1c4b66b1143f4bd
shravaniakude <shravani6akude@gmail.com>
2023-10-08 11:51:59 +0530

    currencyconverter

--- lib/currency_converter.dart
+++ lib/currency_converter.dart
@@ -1,39 +1,50 @@
+// ignore_for_file: dead_code
 
+import 'dart:async';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 // ignore: camel_case_types
-class CurrencyConverter extends StatelessWidget {
+class CurrencyConverter extends StatefulWidget {
   const CurrencyConverter({super.key});
 
   @override
+  State<CurrencyConverter> createState() => _CurrencyConverterstate();
+}
+
+// ignore: unused_element
+class _CurrencyConverterstate extends State<CurrencyConverter> {
+  double result = 0;
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
+    print('rebuilt');
     final border = OutlineInputBorder(
       borderSide: const BorderSide(
-        width :2.0,
-        style :BorderStyle.solid,
-
+        width: 2.0,
+        style: BorderStyle.solid,
       ),
-       borderRadius: BorderRadius.circular(20),
+      borderRadius: BorderRadius.circular(5),
     );
-
 
     return Scaffold(
       backgroundColor: Colors.grey,
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title:const  Text('Currency Converter',
-        style: TextStyle(color: Colors.grey),) ,
+        title: const Text(
+          'Currency Converter',
+          style: TextStyle(color: Colors.grey),
+        ),
         centerTitle: true,
-      ) ,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              '0',
-              style: TextStyle(
+            Text(
+              result.toString(),
+              style: const TextStyle(
                 fontSize: 35,
                 fontWeight: FontWeight.bold,
                 color: Colors.white,
@@ -42,6 +53,10 @@ class CurrencyConverter extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(10.0),
               child: TextField(
+                controller: textEditingController,
+                onSubmitted: (value) {
+                  print(value);
+                },
                 style: const TextStyle(
                   color: Colors.black,
                 ),
@@ -50,7 +65,7 @@ class CurrencyConverter extends StatelessWidget {
                   hintStyle: const TextStyle(
                     color: Colors.black,
                   ),
-                  
+
                   prefixIcon: const Icon(Icons.monetization_on_outlined),
                   prefixIconColor: Colors.black,
                   filled: true, //impstep
@@ -63,9 +78,7 @@ class CurrencyConverter extends StatelessWidget {
                       strokeAlign: BorderSide.strokeAlignOutside,
                     ),
                     borderRadius: BorderRadius.circular(10),
-                    
                   ),
-                  
                 ),
                 keyboardType: const TextInputType.numberWithOptions(
                   decimal: true,
@@ -73,25 +86,22 @@ class CurrencyConverter extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextButton (
-                onPressed: () {
-                  print('button clicked');
-                },
-                style:ElevatedButton.styleFrom(
-                  backgroundColor:Colors.white,
-                  foregroundColor:Colors.black,
-                  fixedSize: const Size(400, 50),
-                      
-                      shape:RoundedRectangleBorder(
+                padding: const EdgeInsets.all(10.0),
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      result = double.parse(textEditingController.text) * 81;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
+                      fixedSize: const Size(400, 50),
+                      shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10),
-                      )
-                      
-                    ),
-                    child: const Text('convert'),
-               )
-    
-            )
+                      )),
+                  child: const Text('convert'),
+                ))
           ],
         ),
       ),
